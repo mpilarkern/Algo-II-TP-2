ac1eb9f057f0d0eb5796744b2fb7517eea73635b
milena-amaro <milenaamaro3@gmail.com>
2024-11-08 19:44:11 -0300

    Update BestEffort.java

--- TP 2/tp2/src/main/java/aed/BestEffort.java
+++ TP 2/tp2/src/main/java/aed/BestEffort.java
@@ -2,21 +2,105 @@ package aed;
 
 import java.util.ArrayList;
 
+import aed.Traslado.GananciasComparator;
+import aed.Traslado.TimeComparator;
+
 public class BestEffort {
-    private Heap<Traslado> trasladosTiempo;
-    private Heap<Traslado> trasladosGanancia;
+    private Heap<Dupla> trasladosTiempo;
+    private Heap<Dupla> trasladosGanancia;
+    private Estadistica despachados;
+    private TimeComparator comparadorTiempo;
+    private GananciasComparator comparadorGanancia;
+
+
+    private class Dupla {
+        Traslado traslado;
+        int handle;
+
+        Dupla(Traslado t, int h) {
+            traslado = t;
+            handle = h;
+            }
+
+        public void CambiarHandle(int h){
+            handle = h;
+        }
+    
+}
 
     public BestEffort(int cantCiudades, Traslado[] traslados){
-        // Implementar
+        trasladosTiempo = new Heap(comparadorTiempo);
+        trasladosGanancia = new Heap(comparadorGanancia);
+        ArrayList<Dupla> listaDuplasTiempo = new ArrayList<Dupla>();
+        ArrayList<Dupla> listaDuplasGanancias = new ArrayList<Dupla>();
+
+        for (int i = 0; i < traslados.length; i++ ){ //creo las duplas que guardan cada traslado y agrego cada una a una lista
+            Traslado t = traslados[i];
+            Dupla duplaTiempo = new Dupla(t,i); 
+            listaDuplasTiempo.add(duplaTiempo);
+
+            Dupla duplaGanancias = new Dupla(t,i); 
+            listaDuplasGanancias.add(duplaGanancias);
+        }
+
+        trasladosTiempo = trasladosTiempo.conjuntoAHeap(listaDuplasTiempo);
+        trasladosGanancia = trasladosGanancia.conjuntoAHeap((listaDuplasGanancias));
+
+        for (int j = 0; j < traslados.length; j++){
+            Dupla duplaTiempo = trasladosTiempo.obtenerElemento(j);
+            int posicionEnLista = duplaTiempo.handle;
+            Dupla duplaGanancias = listaDuplasGanancias.get(posicionEnLista);
+            duplaGanancias.CambiarHandle(j);
+        }
+
+        for (int k = 0; k < traslados.length; k++){
+            Dupla duplaGanancias = trasladosGanancia.obtenerElemento(k);
+            int posicionEnHeapTiempo = duplaGanancias.handle;
+            Dupla duplaTiempo = trasladosTiempo.obtenerElemento(posicionEnHeapTiempo);
+            duplaTiempo.CambiarHandle(k);
+        }
+
+        despachados = new Estadistica(cantCiudades);
+    
+        
     }
 
     public void registrarTraslados(Traslado[] traslados){
-        // Implementar
+        for (int i = 0; i < traslados.length; i++){
+            Dupla dupla = new Dupla(traslados[i],i);
+            int indiceGanancia = trasladosGanancia.agregar(dupla);
+            int indiceTiempo = trasladosTiempo.agregar(dupla);
+
+            int t = trasladosTiempo.tamaño()-1; 
+            while (t > indiceTiempo){            
+                int g = trasladosTiempo.obtenerElemento(t).handle; 
+                Dupla duplaGanancia = trasladosGanancia.obtenerElemento(g);
+                duplaGanancia.CambiarHandle(t);
+                t = (t-1) / 2;  
+            }  
+
+            int g = trasladosGanancia.tamaño()-1; 
+            while (g > indiceGanancia){            
+                t = trasladosGanancia.obtenerElemento(g).handle; 
+                Dupla duplaTiempo = trasladosTiempo.obtenerElemento(t);
+                duplaTiempo.CambiarHandle(g);
+                g = (g-1) / 2;  
+            }  
+        }
     }
 
     public int[] despacharMasRedituables(int n){
-        // Implementar
-        return null;
+        int[] lista_ids = new int[n];
+
+        for (int i = 0; i < n; i++){
+            Dupla maximo = trasladosGanancia.sacarMaximo();
+            int id = maximo.traslado.id;
+            int handle = maximo.handle;
+            lista_ids[i] = id;
+            trasladosTiempo.eliminar(handle);
+
+        }
+        return lista_ids;
     }
 
     public int[] despacharMasAntiguos(int n){
@@ -30,7 +114,6 @@ public class BestEffort {
     }
 
     public ArrayList<Integer> ciudadesConMayorGanancia(){
-        // Implementar
         return null;
     }
 
